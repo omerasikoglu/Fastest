c9d380509f4348e0a26d16648b8b62b8bac1f635
OmerAsik <faruk.asikoglu@gmail.com>
2022-08-28 13:45:55 +0300

    Added: Decorator Pattern

--- Assets/Samples/Architectures/Decorator.cs
+++ Assets/Samples/Architectures/Decorator.cs
@@ -2,35 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Decorator : MonoBehaviour {
+namespace Decorator {
+    public class Decorator : MonoBehaviour {
 
-    private IAbility ability = new DelayedDecorator(new RageAbility());
+        private IAbility ability = new DelayedDecorator(new RageAbility());
 
-    public void Awake() {
-        ability.Use(gameObject);
-    }
-}
-
-public interface IAbility {
-    void Use(GameObject gameObject);
-}
-
-public class DelayedDecorator : IAbility {
-
-    private IAbility wrappedAbility;
-    public DelayedDecorator(IAbility wrappedAbility) {
-        this.wrappedAbility = wrappedAbility;
+        public void Awake() {
+            ability.Use(gameObject);
+        }
     }
 
-    public void Use(GameObject gameObject) {
-        wrappedAbility.Use(gameObject);
-        Debug.Log("DelayedDecorator");
+    public interface IAbility {
+        void Use(GameObject gameObject);
     }
-}
+
+    public class DelayedDecorator : IAbility {
+
+        private IAbility wrappedAbility;
+        public DelayedDecorator(IAbility wrappedAbility) {
+            this.wrappedAbility = wrappedAbility;
+        }
+
+        public void Use(GameObject gameObject) {
+            wrappedAbility.Use(gameObject);
+            Debug.Log("DelayedDecorator");
+        }
+    }
 
 
-public class RageAbility : IAbility {
-    public void Use(GameObject gameObject) {
-        Debug.Log("RageAbility");
-    }
+    public class RageAbility : IAbility {
+        public void Use(GameObject gameObject) {
+            Debug.Log("RageAbility");
+        }
+    } 
 }
